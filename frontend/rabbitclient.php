6f6bc3bd444ce02f41e7883eb09f0e4b3e27a77a
jar243 <jar243@njit.edu>
2021-11-01 12:48:09 -0400

    functional rabbit client with responses

--- frontend/rabbitclient.php
+++ frontend/rabbitclient.php
@@ -16,32 +16,85 @@ class RabbitClient
     {
         $this->host = $host;
         $this->port = $port;
-        $this->user = $pass;
-        $this->port = $port;
+        $this->user = $user;
+        $this->pass = $pass;
     }
 
-    private function openConnection()
+    private function open_connection()
     {
         return new AMQPStreamConnection($this->host, $this->port, $this->user, $this->pass);
     }
 
-    public function login(string $username, string $password)
+    private function publish(string $route, array $args)
     {
-        $connection = $this->openConnection();
+        $connection = $this->open_connection();
         $channel = $connection->channel();
+        $channel->queue_declare($route);
+
+        $json_body = json_encode($args);
+        $msg = new AMQPMessage($json_body);
+
+        $channel->basic_publish($msg, '', $route);
+    }
+
+    private function publish_with_res(string $route, array $args)
+    {
+        $RES_QUEUE = 'amq.rabbitmq.reply-to';
+
+        $connection = $this->open_connection();
+        $channel = $connection->channel();
+
+        $response = null;
+        $channel->basic_consume(
+            $RES_QUEUE,
+            '',
+            false,
+            true,
+            false,
+            false,
+            function (AMQPMessage $message) use (&$response) {
+                $response = json_decode($message->body);
+            },
+        );
+
+        $json_body = json_encode($args);
+
+        $msg = new AMQPMessage(
+            $json_body,
+            [
+                'reply_to' => $RES_QUEUE
+            ],
+        );
+
+        $channel->basic_publish($msg, '', $route);
+        while (is_null($response)) {
+            $channel->wait();
+        }
 
         $channel->close();
         $connection->close();
+
+        return $response;
     }
 
-    public function publish_log(string $logMsg)
+    public function login(string $username, string $password)
     {
-        $connection = $this->openConnection();
+    }
+
+    public function log(string $log_msg)
+    {
+        $connection = $this->open_connection();
         $channel = $connection->channel();
 
         $channel->exchange_declare('logs', 'fanout');
-        $msg = new AMQPMessage($logMsg);
-        $channel->basic_publish($msg, '', 'logs');
+        $body = [
+            'sender' => 'frontend-php',
+            'time' => date('Y/m/d H:i:s'),
+            'msg' => $log_msg,
+        ];
+        $json_body = json_encode($body);
+        $msg = new AMQPMessage($json_body);
+        $channel->basic_publish($msg, 'logs');
 
         $channel->close();
         $connection->close();
