8c64dca48507b388b0234722d9a8d58cd3318ffa
jar243 <jar243@njit.edu>
2021-11-01 12:05:39 -0400

    basic env setup for api

--- backend-api/app.py
+++ backend-api/app.py
@@ -1,18 +1,28 @@
 import requests
 import json
 import pprint
-from requests import api
-from api_data import API_KEY, API_HOST
+from pydantic import BaseSettings
 
-api_key = API_KEY
-api_host = API_HOST
+
+
+class EnvConfig(BaseSettings):
+    tmdb_api_key: str
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+
 
 class MoviesApi:
+    def __init__(self, api_key:str):
+        self._api_key = api_key
+        self._base_url = 'https://api.themoviedb.org'
 
     def search_movies(self, search_str: str):
         # logic here
         search_str = input("Search Movie: ")
-        api_search = "https://api.themoviedb.org/3/search/movie?api_key="+ api_key
+        api_search = f"{self._base_url}/3/search/movie?api_key={self._api_key}"
         params = {'query':search_str}
 
         response = requests.get(api_search, params)
@@ -31,7 +41,7 @@ class MoviesApi:
 
     def trending_movies(self):
 
-        movies_trending = "https://api.themoviedb.org/3/trending/movie/week?api_key=" + api_key
+        movies_trending = "https://api.themoviedb.org/3/trending/movie/week?api_key=" + self._api_key
         response = requests.get(movies_trending)
 
         if response.status_code == 200:
@@ -48,7 +58,7 @@ class MoviesApi:
 
     def trending_shows(self):
     
-        shows_trending = "https://api.themoviedb.org/3/trending/tv/week?api_key=" + api_key
+        shows_trending = "https://api.themoviedb.org/3/trending/tv/week?api_key=" + self._api_key
         response = requests.get(shows_trending)
 
         if response.status_code == 200:
@@ -64,7 +74,7 @@ class MoviesApi:
         return[]
 
     def popular_movies(self):
-        popular = "https://api.themoviedb.org/3/movie/popular?api_key=" + api_key
+        popular = "https://api.themoviedb.org/3/movie/popular?api_key=" + self._api_key
         response = requests.get(popular)
 
         if response.status_code == 200:
@@ -80,7 +90,7 @@ class MoviesApi:
         return[]
 
     def popular_shows(self):
-        popular = "https://api.themoviedb.org/3/tv/popular?api_key=" + api_key
+        popular = "https://api.themoviedb.org/3/tv/popular?api_key=" + self._api_key
         response = requests.get(popular)
 
         if response.status_code == 200:
@@ -95,13 +105,14 @@ class MoviesApi:
             print(f"Error: {response.status_code} ")
         return[]
 
-search_movie = MoviesApi()
+
+config = EnvConfig()
+api = MoviesApi(config.tmdb_api_key)
+
 #search_movie.search_movies("")
 
-trending = MoviesApi()
-all = trending.trending_shows()
-#print(all)
+trending = api.trending_shows()
+print(trending)
 
-popular = MoviesApi()
-pop = popular.popular_shows()
-#print(pop)
+popular = api.popular_shows()
+print(popular)
