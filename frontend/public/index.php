7e4f21291bb6da643ff0b8e4b57b1a17e0f154f9
jar243 <jar243@njit.edu>
2021-11-02 17:04:24 -0400

    reusable header and footer with navbar

---
+++ frontend/public/index.php
@@ -0,0 +1,9 @@
+<?php
+include('protected/header.php');
+?>
+
+<h1>Welcome to the Movies App!</h1>
+
+<?php
+include('protected/footer.php');
+?>
---
+++ frontend/public/protected/header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body class="bg-secondary p-2">
+
+  <nav class="navbar navbar-dark bg-dark navbar-expand-lg mb-3">
+
+    <div class="navbar-brand">Movies App</div>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleTarget">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarToggleTarget">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+          <a class="nav-link" href=".">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./login.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="./registration.php">Registration</a>
+        </li>
+      </ul>
+    </div>
+
+  </nav>
