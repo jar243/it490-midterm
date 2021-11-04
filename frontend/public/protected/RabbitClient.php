79cd56a4ffe3a96ef884d654476c6a8a513ab0c2
jar243 <jar243@njit.edu>
2021-11-04 15:02:00 -0400

    frontend client - user update feature

--- frontend/public/protected/RabbitClient.php
+++ frontend/public/protected/RabbitClient.php
@@ -111,6 +111,18 @@ class RabbitClient
         );
     }
 
+    public function user_update(string $token, string $display_name, string $bio)
+    {
+        return $this->publish(
+            'db.user.update',
+            [
+                'token' => $token,
+                'display_name' => $display_name,
+                'bio' => $bio
+            ]
+        );
+    }
+
     public function token_generate(string $username, string $password)
     {
         return $this->publish(
@@ -121,16 +133,15 @@ class RabbitClient
 
     public function token_get_user(string $token)
     {
-        $res = $this->publish(
+        return $this->publish(
             'db.token.get-user',
             ['token' => $token]
         );
-        return $res;
     }
 
     public function token_delete(string $token)
     {
-        $this->publish(
+        return $this->publish(
             'db.token.delete',
             ['token' => $token]
         );
