8c423b1de3f7013d3fe6b5e32f34f701eb52c8a5
jar243 <jar243@njit.edu>
2021-11-04 14:47:14 -0400

    started creating user profile pages

---
+++ frontend/public/profile.php
@@ -0,0 +1,21 @@
+<?php
+include('protected/header.php');
+
+if (isset($_GET['username'])) {
+    $un = $_GET['username'];
+    if (!is_null($active_user) && $active_user->username == $un) {
+        header("location: my-profile.php");
+        exit();
+    }
+} else {
+    header("location: index.php");
+    exit();
+}
+
+?>
+
+
+
+<?php
+include('protected/footer.php');
+?>
