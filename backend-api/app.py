70d335f57dffaf2a1e197abc2c7d972c66cc8dfd
jar243 <jar243@njit.edu>
2021-11-07 18:25:30 -0500

    example api handlers and setup

--- backend-api/app.py
+++ backend-api/app.py
@@ -9,37 +9,38 @@ class EnvConfig(BaseSettings):
     tmdb_api_key: str
 
     class Config:
-        env_file = '.env'
-        env_file_encoding = 'utf-8'
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
 
 class MoviesApi:
-    def __init__(self, api_key:str):
+    def __init__(self, api_key: str):
         self._api_key = api_key
-        self._host_url = 'https://api.themoviedb.org'
-        self._image_url = 'https://image.tmdb.org/t/p/w500/'
+        self._host_url = "https://api.themoviedb.org"
+        self._image_url = "https://image.tmdb.org/t/p/w500/"
 
     def search_movies(self, search_str: str):
         # logic here
         search_str = input("Search Movie: ")
         api_search = f"{self._host_url}/3/search/movie?api_key={self._api_key}"
 
-        params = {'query': search_str}
+        params = {"query": search_str}
 
         response = requests.get(api_search, params)
 
-        # request data and check for error 
+        # request data and check for error
 
         if response.status_code == 200:
             data = json.loads(response.text)
-            
-            for i in data['results']:  
-                print('Title: ' + i['title'] +'\n')
-                print('Overview: ' + i['overview']+'\n')
 
-                poster_path = (i['backdrop_path'])
+            for i in data["results"]:
+                print("Title: " + i["title"] + "\n")
+                print("Overview: " + i["overview"] + "\n")
+
+                poster_path = i["backdrop_path"]
                 if poster_path != None:
                     print(f"{self._image_url}{poster_path}")
-                    
+
                 else:
                     print("Sorry, poster not available")
 
@@ -49,17 +50,19 @@ class MoviesApi:
 
     def trending_movies(self):
 
-        movies_trending = f"{self._host_url}/3/trending/movie/week?api_key=" + self._api_key
+        movies_trending = (
+            f"{self._host_url}/3/trending/movie/week?api_key=" + self._api_key
+        )
         response = requests.get(movies_trending)
-    
+
         if response.status_code == 200:
             data = json.loads(response.text)
-        
-            for i in data['results']:
-                print('Title: ' + i['title'] + '\n')
-                print('Overview: ' + i['overview']+'\n')
 
-                poster_path = (i['backdrop_path'])
+            for i in data["results"]:
+                print("Title: " + i["title"] + "\n")
+                print("Overview: " + i["overview"] + "\n")
+
+                poster_path = i["backdrop_path"]
                 if poster_path != None:
                     print(f"{self._image_url}{poster_path}")
                 else:
@@ -68,30 +71,24 @@ class MoviesApi:
         else:
             print(f"Error: {response.status_code} ")
 
-    
         return ()
-    
 
     def trending_shows(self):
-    
         shows_trending = f"{self._host_url}/3/trending/tv/week?api_key=" + self._api_key
         response = requests.get(shows_trending)
 
         if response.status_code == 200:
             data = json.loads(response.text)
-            
-            for i in data['results']:
-                print('Name: ' + i['name'] + '\n')
-                print('Overview: ' + i['overview']+'\n')
-                poster_path = (i['backdrop_path'])
-                if poster_path != None:
-                    print(f"{self._image_url}{poster_path}")
-                else:
-                    print("Sorry, poster not available")
-
+            return [
+                {
+                    "title": movie["name"],
+                    "description": movie["overview"],
+                    "poster_path": f"{self._image_url}{movie['backdrop_path']}",
+                }
+                for movie in data["results"]
+            ]
         else:
-            print(f"Error: {response.status_code} ")
-        return[]
+            raise RuntimeError(f"API Error: {response.status_code}")
 
     def popular_movies(self):
         popular = f"{self._host_url}/3/movie/popular?api_key=" + self._api_key
@@ -99,11 +96,11 @@ class MoviesApi:
 
         if response.status_code == 200:
             data = json.loads(response.text)
-            
-            for i in data['results']:
-                print('Title: ' + i['title'] + '\n')
-                print('Overview: ' + i['overview']+'\n')
-                poster_path = (i['backdrop_path'])
+
+            for i in data["results"]:
+                print("Title: " + i["title"] + "\n")
+                print("Overview: " + i["overview"] + "\n")
+                poster_path = i["backdrop_path"]
                 if poster_path != None:
                     print(f"{self._image_url}{poster_path}")
                 else:
@@ -111,7 +108,7 @@ class MoviesApi:
 
         else:
             print(f"Error: {response.status_code} ")
-        return[]
+        return []
 
     def popular_shows(self):
         popular = f"{self._host_url}/3/tv/popular?api_key=" + self._api_key
@@ -119,38 +116,36 @@ class MoviesApi:
 
         if response.status_code == 200:
             data = json.loads(response.text)
-            
-            for i in data['results']:
-                print('Name: ' + i['name'] + '\n')
-                print('Overview: ' + i['overview']+'\n')
-                poster_path = (i['backdrop_path'])
+
+            for i in data["results"]:
+                print("Name: " + i["name"] + "\n")
+                print("Overview: " + i["overview"] + "\n")
+                poster_path = i["backdrop_path"]
                 if poster_path != None:
                     print(f"{self._image_url}{poster_path}")
                 else:
                     print("Sorry, poster not available")
         else:
             print(f"Error: {response.status_code} ")
-        return[]
+        return []
 
 
 api: MoviesApi
 
-def handle_movies_search(req_body: dict):
-    query = req_body.get('query')
-    if query is None or not isinstance(query, str) or len(query) == 0:
-        raise RuntimeError('Invalid query supplied')
-    else:
-        MoviesApi.search_movies(query)
-        results = []
 
-    return {'results': results}
+def handle_movies_search(req_body: dict):
+    query = req_body.get("query")
+    if query is None or not isinstance(query, str) or len(query) == 0:
+        raise RuntimeError("Invalid query supplied")
+
+    results = api.search_movies(query)
+    return {"movies": results}
+
 
 def handle_trending_movies(req_body: dict):
-    rq = MoviesApi(**req_body)
-    rq.trending_movies()
-    results = []
+    results = api.trending_movies()
+    return {"movies": results}
 
-    return {'results': results}
 
 def handle_trending_shows(req_body: dict):
     rq = MoviesApi(**req_body)
@@ -158,40 +153,35 @@ def handle_trending_shows(req_body: dict):
 
     results = []
 
-    return {'results': results}
+    return {"results": results}
+
 
 def handle_popular_movies(req_body: dict):
     results = []
-    return {'results' : results}
+    return {"results": results}
+
 
 def handle_popular_shows(req_body: dict):
     results = []
-    return {'results' : results}
+    return {"results": results}
+
 
 def main():
 
     routes = {
-        "api.movies.search": handle_movies_search,  
-        "api.trending.movies": handle_trending_movies, 
-        "api.trending.shows" : handle_trending_shows,
-        "api.popular.movies" : handle_popular_movies,
-        "api.popular.shows" : handle_popular_shows
+        "api.movies.search": handle_movies_search,
+        "api.trending.movies": handle_trending_movies,
+        "api.trending.shows": handle_trending_shows,
+        "api.popular.movies": handle_popular_movies,
+        "api.popular.shows": handle_popular_shows,
     }
 
-    trending_movies = {
-        'api.trending.movies' : handle_trending_movies
-    }
-    trending_shows = {
-        'api.trending.shows' : handle_trending_shows
-    }
-    popular_movies = {
-        'api.popular.movies' : handle_popular_movies
-    }
-    popular_shows = {
-        'api.popular.shows' : handle_popular_shows
-    }
+    trending_movies = {"api.trending.movies": handle_trending_movies}
+    trending_shows = {"api.trending.shows": handle_trending_shows}
+    popular_movies = {"api.popular.movies": handle_popular_movies}
+    popular_shows = {"api.popular.shows": handle_popular_shows}
     global api
     config = EnvConfig()
     api = MoviesApi(config.tmdb_api_key)
 
-    run_rabbit_app('backend-api', '127.0.0.1', 5672, 'guest', 'guest', routes)
+    run_rabbit_app("backend-api", "127.0.0.1", 5672, "guest", "guest", routes)
