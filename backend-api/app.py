10ed5b28cff1ab1c4b91366970880b4c1c2b05b2
jar243 <jar243@njit.edu>
2021-11-08 00:11:40 -0500

    backend api bugfixes

--- backend-api/app.py
+++ backend-api/app.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 from pydantic import BaseSettings
-from broker import run_rabbit_app
+from broker import run_rabbit_app, UserError
 
 
 class EnvConfig(BaseSettings):
@@ -19,98 +19,54 @@ class MoviesApi:
         self._host_url = "https://api.themoviedb.org"
         self._image_url = "https://image.tmdb.org/t/p/w500/"
 
+    def _format_movie_data(self, raw_movie: dict):
+        return {
+            "id": raw_movie["id"],
+            "title": raw_movie["title"],
+            "description": raw_movie["overview"],
+            "genre_ids": raw_movie["genre_ids"],
+            "year": raw_movie["release_date"][:4],
+            "poster_path": f"{self._image_url}{raw_movie['poster_path'][1:]}",
+        }
+
     def search_movies(self, search_str: str):
-        # logic here
-        search_str = input("Search Movie: ")
         api_search = f"{self._host_url}/3/search/movie?api_key={self._api_key}"
-
         params = {"query": search_str}
-
         response = requests.get(api_search, params)
 
-        # request data and check for error
-
         if response.status_code == 200:
             data = json.loads(response.text)
-            return [
-                {
-                    "title": movie["name"],
-                    "description": movie["overview"],
-                    "poster_path": f"{self._image_url}{movie['backdrop_path']}",
-                }
-                for movie in data["results"]
-            ]
+            return [self._format_movie_data(movie) for movie in data["results"]]
         else:
             raise RuntimeError(f"API Error: {response.status_code}")
+
+    def get_movie(self, movie_id: str):
+        path = f"{self._host_url}/3/movie/{movie_id}?api_key={self._api_key}"
+        res = requests.get(path)
+        if res.status_code == 200:
+            return self._format_movie_data(json.loads(res.text))
+        elif res.status_code == 404:
+            raise UserError(f"Movies ID does not exist: {movie_id}")
+        else:
+            raise RuntimeError(f"API Error: {res.status_code}")
 
     def trending_movies(self):
-
-        movies_trending = (
-            f"{self._host_url}/3/trending/movie/week?api_key=" + self._api_key
-        )
-        response = requests.get(movies_trending)
+        path = f"{self._host_url}/3/trending/movie/week?api_key={self._api_key}"
+        response = requests.get(path)
 
         if response.status_code == 200:
             data = json.loads(response.text)
-            return [
-                {
-                    "title": movie["name"],
-                    "description": movie["overview"],
-                    "poster_path": f"{self._image_url}{movie['backdrop_path']}",
-                }
-                for movie in data["results"]
-            ]
-        else:
-            raise RuntimeError(f"API Error: {response.status_code}")
-
-    def trending_shows(self):
-        shows_trending = f"{self._host_url}/3/trending/tv/week?api_key=" + self._api_key
-        response = requests.get(shows_trending)
-
-        if response.status_code == 200:
-            data = json.loads(response.text)
-            return [
-                {
-                    "title": movie["name"],
-                    "description": movie["overview"],
-                    "poster_path": f"{self._image_url}{movie['backdrop_path']}",
-                }
-                for movie in data["results"]
-            ]
+            return [self._format_movie_data(movie) for movie in data["results"]]
         else:
             raise RuntimeError(f"API Error: {response.status_code}")
 
     def popular_movies(self):
-        popular = f"{self._host_url}/3/movie/popular?api_key=" + self._api_key
-        response = requests.get(popular)
+        path = f"{self._host_url}/3/movie/popular?api_key={self._api_key}"
+        response = requests.get(path)
 
         if response.status_code == 200:
             data = json.loads(response.text)
-            return [
-                {
-                    "title": movie["name"],
-                    "description": movie["overview"],
-                    "poster_path": f"{self._image_url}{movie['backdrop_path']}",
-                }
-                for movie in data["results"]
-            ]
-        else:
-            raise RuntimeError(f"API Error: {response.status_code}")
-
-    def popular_shows(self):
-        popular = f"{self._host_url}/3/tv/popular?api_key=" + self._api_key
-        response = requests.get(popular)
-
-        if response.status_code == 200:
-            data = json.loads(response.text)
-            return [
-                {
-                    "title": movie["name"],
-                    "description": movie["overview"],
-                    "poster_path": f"{self._image_url}{movie['backdrop_path']}",
-                }
-                for movie in data["results"]
-            ]
+            return [self._format_movie_data(movie) for movie in data["results"]]
         else:
             raise RuntimeError(f"API Error: {response.status_code}")
 
@@ -118,10 +74,17 @@ class MoviesApi:
 api: MoviesApi
 
 
+def handle_movies_get(req_body: dict):
+    movie_id = req_body.get("movie_id")
+    if movie_id is None or not isinstance(movie_id, str) or len(movie_id) == 0:
+        raise ValueError("Invalid movie_id supplied")
+    return api.get_movie(movie_id)
+
+
 def handle_movies_search(req_body: dict):
     query = req_body.get("query")
     if query is None or not isinstance(query, str) or len(query) == 0:
-        raise RuntimeError("Invalid query supplied")
+        raise ValueError("Invalid query supplied")
 
     results = api.search_movies(query)
     return {"movies": results}
@@ -132,28 +95,17 @@ def handle_trending_movies(req_body: dict):
     return {"movies": results}
 
 
-def handle_trending_shows(req_body: dict):
-    results = api.trending_movies()
-    return {"shows": results}
-
-
 def handle_popular_movies(req_body: dict):
-    results = api.trending_movies()
+    results = api.popular_movies()
     return {"movies": results}
-
-
-def handle_popular_shows(req_body: dict):
-    results = api.trending_movies()
-    return {"shows": results}
 
 
 def main():
     routes = {
+        "api.movies.get": handle_movies_get,
         "api.movies.search": handle_movies_search,
         "api.trending.movies": handle_trending_movies,
-        "api.trending.shows": handle_trending_shows,
         "api.popular.movies": handle_popular_movies,
-        "api.popular.shows": handle_popular_shows,
     }
 
     global api
