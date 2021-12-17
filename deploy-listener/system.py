a9d48f0016c4011030b9a97452d76ac54f343847
DIVYANGIP <89935711+DIVYANGIP@users.noreply.github.com>
2021-12-16 21:01:28 -0500

    Update system.py

--- deploy-listener/system.py
+++ deploy-listener/system.py
@@ -8,7 +8,7 @@ def Status():
     p = subprocess.run(['service', 'it490-backend','status'], capture_output=True)
     print(p.stdout.decode('utf-8'))
 
-def Stop():s
+def Stop():
     p = subprocess.run(['service', 'it490-backend','stop'], capture_output=True)
     print(p.stdout.decode('utf-8'))
 
